ebac5a5893f4ae08196b06f768d91fd10712fe45
skdiawotjd <skdiawotjd12@naver.com>
2022-08-09 21:41:30 +0900

    Check How To Collision by layer

---
+++ Assets/Script/Tem/TemPortal.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemPortal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "R_Weapon")
+        {
+            MoveScene(2);
+        }
+    }
+
+    public void MoveScene(int SceneType)
+    {
+        switch (SceneType)
+        {
+            case 2:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("TestScene");
+                break;
+        }
+    }
+}

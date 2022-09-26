b3e7088adaaa91a7fd49fc78cff493df877c11e1
skdiawotjd <skdiawotjd12@naver.com>
2022-09-27 01:58:54 +0900

    complete Follow Camera(Move Scene)

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,34 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
-    // 캐릭터의 Transform
-    private Transform playerTransform;
-    // z축 땜에
+    // 씬의 크기
+    private RectTransform Background;
+    // 카메라 기본 위치
     private Vector3 cameraPosition;
-
+    
+    private Vector3 RangePosition;
+    
     private Vector2 center;
 
+    // 현재 씬 크기
     [SerializeField]
-    Vector2 mapSize;
+    private Vector2 MapSize;
 
     [SerializeField]
     // 카메라 따라가는 속도
-    public float cameraSpeed;
+    private float cameraSpeed;
     private float height;
     private float width;
 
     void Start()
     {
-        cameraPosition = new Vector3(0, 0, -10);
-        cameraSpeed = 5.0f;
+        DontDestroyOnLoad(this);
 
-        playerTransform = Character.instance.GetComponent<Transform>();
+        SceneManager.sceneLoaded += LoadedsceneEvent;
+
+        cameraPosition = new Vector3(0, 0, -10f);
+        RangePosition = new Vector3(0, 0, -10f);
+        cameraSpeed = 5.0f;
 
         height = Camera.main.orthographicSize;
         width = height * Screen.width / Screen.height;
+
+        LimitCameraArea();
     }
 
     void FixedUpdate()
@@ -37,23 +46,35 @@ public class CameraController : MonoBehaviour
         LimitCameraArea();
     }
 
-    void LimitCameraArea()
+    private void LimitCameraArea()
     {
-        /*transform.position = Vector3.Lerp(transform.position, playerTransform.position + cameraPosition, Time.deltaTime * cameraSpeed);*/
         transform.position = Vector3.Lerp(transform.position, Character.instance.transform.position + cameraPosition, Time.deltaTime * cameraSpeed);
 
-        float lx = mapSize.x - width;
-        float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
 
-        float ly = mapSize.y - height;
-        float clampY = Mathf.Clamp(transform.position.y, -ly + center.y, ly + center.y);
+        RangePosition.x = Mathf.Clamp(transform.position.x, center.x - (MapSize.x - width), (MapSize.x - width) + center.x);
+        RangePosition.y = Mathf.Clamp(transform.position.y, center.y - (MapSize.y - height), (MapSize.y - height) + center.y);
 
-        transform.position = new Vector3(clampX, clampY, -10f);
+        transform.position = RangePosition;
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(center, mapSize * 2);
+        Gizmos.DrawWireCube(center, MapSize * 2);
+    }
+
+    private void LoadedsceneEvent(Scene scene, LoadSceneMode mode)
+    {
+        Background = GameObject.Find("Background").GetComponent<RectTransform>();
+
+        if ((MapSize * 2) != Background.sizeDelta)
+        {
+            MapSize = Background.sizeDelta / 2f;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, Character.instance.transform.position + cameraPosition, 1f);
+
+        Debug.Log(scene.name + "으로 변경되었습니다.");
     }
 }

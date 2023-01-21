287483038c77db89d3a8749e60051c54f324cb8e
skdiawotjd <skdiawotjd12@naver.com>
2023-01-21 20:44:53 +0900

    Problem NotPushing JustChatNpc.cs 2

--- Assets/Script/Npc/JustChatNPC.cs
+++ Assets/Script/Npc/JustChatNPC.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TemNPC : BasicNpc
+public class JustChatNPC : BasicNpc
 {
     protected override void Start()
     {
